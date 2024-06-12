0edb442e5a1c5122fd641d05ad09f50696fc9514
holarohit <rohit.kitkat2@gmail.com>
2024-06-12 12:20:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
+import MovieCard from './MovieCard';
 import './App.css';
+import SearchIcon from './search.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+//49c7d197
+
+const API_URL ='http://www.omdbapi.com?apikey=49c7d197';
+
+
+const App = ()=>{
+  const [movies ,setMovies] = useState([]);
+  const [searchTerm ,setSerachTerm] = useState('');
+  const searchMovies = async(title) => {
+    const response =await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+
+  }
+  useEffect(()=>{
+  
+   searchMovies('')
+   
+
+  },[]);
+
+  return(
+   <div className="app">
+    <h1>Hola TV</h1>
+      <div className="search">
+        <input
+           placeholder="search for movies"
+           value={searchTerm}
+           onChange={(e)=> setSerachTerm (e.target.value)}
+        />
+
+        <img src={SearchIcon}
+        alt="search"
+        onClick={()=> searchMovies(searchTerm)}
+        />
+
+      </div>
+      {
+         movies?.length >0
+         ? (
+            
+        <div className="container">
+     
+        {movies.map((movie)=>(
+
+           <MovieCard movie={movie}/>
+
+
+        ))}
+    
+     </div>
+
+         ):(
+           <div className="empty">
+            <h2> Welcome Dude </h2>
+
+            </div>
+
+
+         )
+
+
+      }
+      
+
+
+
+      
+
+     
+
+
+   </div>
   );
 }
-
+//#010136 input box color
 export default App;
+
